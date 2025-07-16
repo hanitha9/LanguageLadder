62c136a9ece326e6d34a340596ec558f60453d1e
Hanitha Ganisetti <149133841+hanitha9@users.noreply.github.com>
2025-07-16 21:04:44 +0530

    Create avatar.tsx

---
+++ components/ui/avatar.tsx
@@ -0,0 +1,43 @@
+```tsx
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface AvatarProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+Avatar.displayName = "Avatar";
+
+export interface AvatarImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  className?: string;
+}
+
+const AvatarImage = React.forwardRef<HTMLImageElement, AvatarImageProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <img
+        ref={ref}
+        className={cn("aspect-square h-full w-full object-cover", className)}
+        {...props}
+      />
+    );
+  }
+);
+AvatarImage.displayName = "AvatarImage";
+
+export { Avatar, AvatarImage };
+```
