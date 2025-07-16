f3e42d6f06c8deeff4e3eb60c7d960b8365fe446
Hanitha Ganisetti <149133841+hanitha9@users.noreply.github.com>
2025-07-16 21:03:49 +0530

    Create seperator.tsx

---
+++ components/ui/seperator.tsx
@@ -0,0 +1,27 @@
+```tsx
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation?: "horizontal" | "vertical";
+}
+
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, orientation = "horizontal", ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          orientation === "horizontal" ? "h-[2px] w-full" : "h-full w-[2px]",
+          "bg-neutral-200 rounded-full",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+Separator.displayName = "Separator";
+
+export { Separator };
+```
